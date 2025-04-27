43a80ee205d229d52eb90edfc05e8e931e390d19
dev-Chaves <chavesprogrammer@gmail.com>
2025-04-27 20:37:58 -0300

    inputs+forms

--- src/app/components/sem-loggar/login/login.component.ts
+++ src/app/components/sem-loggar/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-sem-loggar',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.loginForm = this.formBuilder.group({
+      usersName: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+    }
+  }
 
 }
