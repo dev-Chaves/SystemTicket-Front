c969b6b2ea9ce8eba3f963e4d5070f0887f48c59
dev-Chaves <chavesprogrammer@gmail.com>
2025-04-27 20:38:47 -0300

    reactive formules e ajustes no gap

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/sem-loggar/header/header.component";
 import { LoginComponent } from '../../components/sem-loggar/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, LoginComponent],
+  imports: [HeaderComponent, LoginComponent, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 } )
