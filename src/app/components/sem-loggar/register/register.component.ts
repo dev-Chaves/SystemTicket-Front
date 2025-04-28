6cb3f67b68bb9b6c08a7b4e23ff826438cf0564e
dev-Chaves <chavesprogrammer@gmail.com>
2025-04-27 21:08:36 -0300

    layout register

--- src/app/components/sem-loggar/register/register.component.ts
+++ src/app/components/sem-loggar/register/register.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-sem-loggar',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponentSemLoggar {
+  
+  registerForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder){
+
+    this.registerForm = this.formBuilder.group({
+      usersName: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      token: ['', [Validators.required, Validators.minLength(3)]]
+    })
+
+  }
+
+
+  onSubmit(){
+    if(this.registerForm.valid){
+      console.log(this.registerForm.value);
+    }
+  }
 
 }
