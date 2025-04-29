297cda38bccbba1f93a2e5affdd0c5b223fb7fda
dev-Chaves <chavesprogrammer@gmail.com>
2025-04-29 18:01:00 -0300

    fixed basic shit

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { TicketListComponent } from '../../components/loggado/ticket-list/ticket-list.component';
+import { CardsComponent } from '../../components/loggado/cards/cards.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TicketListComponent],
+  imports: [
+    TicketListComponent, 
+    CardsComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
