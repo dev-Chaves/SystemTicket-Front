819f487a0a1e1eca18359e0b0ec187fda1026218
dev-Chaves <chavesprogrammer@gmail.com>
2025-05-04 22:16:17 -0300

    removendo lógica do modal antigo

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/loggado/sidebar/sidebar.component';
-import { CardsComponent } from '../../components/loggado/cards/cards.component';
 import { TicketListComponent } from '../../components/loggado/ticket-list/ticket-list.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  imports: [SidebarComponent, CardsComponent, TicketListComponent, CardsComponent, SidebarComponent],
+  imports: [SidebarComponent, TicketListComponent],
   standalone: true
 })
 export class DashboardComponent {
-
 }
